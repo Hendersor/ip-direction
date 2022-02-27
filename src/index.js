89176fe37ff3893e4bcbab34c1602c105b082d86
Francisco <montejof1998@gmail.com>
2022-02-27 14:02:32 -0800

    We added the map to the app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import "./styles/main.scss"
+import "./styles/main.scss";
+
+let map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer(
+  "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: "mapbox/streets-v11",
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken:
+      "pk.eyJ1IjoiaGVuZGVyc29yIiwiYSI6ImNsMDV0ODZ0cjE2amgza3FpZm55MDFmYnkifQ.HuzZnfKV63lmuIbUn4mIpw",
+  }
+).addTo(map);
